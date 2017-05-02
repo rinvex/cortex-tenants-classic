802d062b9de4f4bcb45846187be58797fdc35629
Abdelrahman Omran <me@omranic.com>
2017-05-02 14:42:04 +0200

    Fix wrong identifier in tenant form

--- resources/views/backend/forms/tenant.blade.php
+++ resources/views/backend/forms/tenant.blade.php
@@ -3,7 +3,7 @@
 
 {{-- Page Title --}}
 @section('title')
-    {{ config('app.name') }} » {{ trans('cortex/foundation::common.backend') }} » {{ trans('cortex/tenantable::common.tenants') }} » {{ $tenant->exists ? $tenant->username : trans('cortex/tenantable::common.create_tenant') }}
+    {{ config('app.name') }} » {{ trans('cortex/foundation::common.backend') }} » {{ trans('cortex/tenantable::common.tenants') }} » {{ $tenant->exists ? $tenant->slug : trans('cortex/tenantable::common.create_tenant') }}
 @stop
 
 @push('scripts')
@@ -51,11 +51,11 @@
     <div class="content-wrapper">
         <!-- Breadcrumbs -->
         <section class="content-header">
-            <h1>{{ $tenant->exists ? $tenant->username : trans('cortex/tenantable::common.create_tenant') }}</h1>
+            <h1>{{ $tenant->exists ? $tenant->slug : trans('cortex/tenantable::common.create_tenant') }}</h1>
             <ol class="breadcrumb">
                 <li><a href="{{ route('backend.home') }}"><i class="fa fa-dashboard"></i> {{ trans('cortex/foundation::common.backend') }}</a></li>
                 <li><a href="{{ route('backend.tenants.index') }}">{{ trans('cortex/tenantable::common.tenants') }}</a></li>
-                <li class="active">{{ $tenant->exists ? $tenant->username : trans('cortex/tenantable::common.create_tenant') }}</li>
+                <li class="active">{{ $tenant->exists ? $tenant->slug : trans('cortex/tenantable::common.create_tenant') }}</li>
             </ol>
         </section>
 
