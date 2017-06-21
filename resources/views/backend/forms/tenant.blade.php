a2b54f230f1c06b6e0b811f806285ac20aec999f
Abdelrahman Omran <me@omranic.com>
2017-06-22 00:47:07 +0200

    Add slugify support

--- resources/views/backend/forms/tenant.blade.php
+++ resources/views/backend/forms/tenant.blade.php
@@ -88,7 +88,7 @@
                                     {{-- Name --}}
                                     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                         {{ Form::label('name', trans('cortex/tenantable::common.name'), ['class' => 'control-label']) }}
-                                        {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => trans('cortex/tenantable::common.name'), 'required' => 'required', 'autofocus' => 'autofocus']) }}
+                                        {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => trans('cortex/tenantable::common.name'), 'data-slugify' => '#slug', 'required' => 'required', 'autofocus' => 'autofocus']) }}
 
                                         @if ($errors->has('name'))
                                             <span class="help-block">{{ $errors->first('name') }}</span>
