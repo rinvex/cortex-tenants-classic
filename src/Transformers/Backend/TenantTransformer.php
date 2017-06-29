edac95df8abde857c2b0f3b4bba8a230e52c09ca
Abdelrahman Omran <me@omranic.com>
2017-06-29 14:01:14 +0200

    Fix country and language attribute issues after core package update

--- src/Transformers/Backend/TenantTransformer.php
+++ src/Transformers/Backend/TenantTransformer.php
@@ -21,8 +21,8 @@ class TenantTransformer extends TransformerAbstract
             'phone' => (string) $tenant->phone,
             'address' => (string) $tenant->address,
             'owner' => (string) $tenant->owner->username,
-            'country_code' => (string) $tenant->country_code ? $tenant->country->getName().'&nbsp;&nbsp;'.$tenant->country->getEmoji() : '',
-            'language_code' => (string) $tenant->language_code ? $tenant->language->getName() : '',
+            'country_code' => (string) $tenant->country_code ? country($tenant->country_code)->getName().'&nbsp;&nbsp;'.country($tenant->language_code)->getEmoji() : '',
+            'language_code' => (string) $tenant->language_code ? language($tenant->language_code)->getName() : '',
             'created_at' => (string) $tenant->created_at,
             'updated_at' => (string) $tenant->updated_at,
         ];
