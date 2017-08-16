44f5016625f8fd93b54f4022637e9af1a8b82419
Abdelrahman Omran <me@omranic.com>
2017-08-16 15:51:45 +0200

    Register breadcrumbs

--- src/Providers/TenantableServiceProvider.php
+++ src/Providers/TenantableServiceProvider.php
@@ -33,6 +33,7 @@ class TenantableServiceProvider extends ServiceProvider
     public function boot(Router $router)
     {
         // Load resources
+        require __DIR__.'/../../routes/breadcrumbs.php';
         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'cortex/tenantable');
         $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'cortex/tenantable');
