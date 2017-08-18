a931ef75e2d59e9ce2ecebaa27394900bdd8cc89
Abdelrahman Omran <me@omranic.com>
2017-08-18 22:44:38 +0200

    Tweak seeder's output

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -35,6 +35,8 @@ class SeedCommand extends Command
      */
     public function handle()
     {
+        $this->warn('Seed cortex/tenantable:');
+
         if ($this->ensureExistingTenantableTables()) {
             // No seed data at the moment!
         }
