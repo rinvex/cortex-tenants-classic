7edea58ae253a45561dfa5dd916d2ba0399046d5
Abdelrahman Omran <me@omranic.com>
2017-12-13 19:59:59 +0200

    Replace menu toggle link

--- resources/views/managerarea/partials/header.blade.php
+++ resources/views/managerarea/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="{{ route('managerarea.home') }}" class="logo">
+    <a href="#" class="logo" data-toggle="push-menu" role="button">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><i class="fa fa-home"></i></span>
         <!-- logo for regular state and mobile devices -->
@@ -8,13 +8,9 @@
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-            <span class="sr-only">{{ trans('cortex/foundation::common.toggle_navigation') }}</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </a>
+        <ul class="nav navbar-nav">
+            <li><a href="{{ route('adminarea.home') }}"><i class="fa fa-home"></i> {{ trans('cortex/foundation::common.home') }}</a></li>
+        </ul>
 
         <div class="navbar-custom-menu">
             {!! Menu::managerareaTopbar()->addClass('nav navbar-nav')->setActiveFromRequest() !!}
