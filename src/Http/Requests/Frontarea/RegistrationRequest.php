d5556d9edb6a3798a0de24cd8ec5b981b44f004f
Abdelrahman Omran <me@omranic.com>
2018-02-11 14:30:24 +0200

    Move GenericException to cortex/foundation from rinvex/fort

--- src/Http/Requests/Frontarea/RegistrationRequest.php
+++ src/Http/Requests/Frontarea/RegistrationRequest.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Cortex\Tenants\Http\Requests\Frontarea;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Rinvex\Fort\Exceptions\GenericException;
+use Cortex\Foundation\Exceptions\GenericException;
 
 class RegistrationRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      *
-     * @throws \Rinvex\Fort\Exceptions\GenericException
+     * @throws \Cortex\Foundation\Exceptions\GenericException
      *
      * @return bool
      */
