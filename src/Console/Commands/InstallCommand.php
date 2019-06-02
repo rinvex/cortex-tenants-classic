3e788bfe40eb1265adcfae46ae5ea6153a7b854d
Abdelrahman Omran <me@omranic.com>
2019-06-02 05:24:45 +0200

    Refactor migrations and artisan commands, and tweak service provider publishes functionality

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -29,10 +29,10 @@ class InstallCommand extends Command
      */
     public function handle(): void
     {
-        $this->warn($this->description);
+        $this->alert($this->description);
 
-        $this->call('cortex:migrate:tenants', ['--force' => $this->option('force')]);
         $this->call('cortex:publish:tenants', ['--force' => $this->option('force')]);
+        $this->call('cortex:migrate:tenants', ['--force' => $this->option('force')]);
         $this->call('cortex:seed:tenants');
     }
 }
--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -31,6 +31,16 @@ class MigrateCommand extends BaseMigrateCommand
     {
         parent::handle();
 
-        $this->call('migrate', ['--step' => true, '--path' => 'app/cortex/tenants/database/migrations', '--force' => $this->option('force')]);
+        if (file_exists($path = 'database/migrations/cortex/tenants')) {
+            $this->call('migrate', [
+                '--step' => true,
+                '--path' => $path,
+                '--force' => $this->option('force'),
+            ]);
+        } else {
+            $this->warn('No migrations found! Consider publish them first: <fg=green>php artisan cortex:publish:tenants</>');
+        }
+
+        $this->line('');
     }
 }
