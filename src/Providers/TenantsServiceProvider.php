10497a6439ab37b0547a0af9f0daa06ffda444fe
Abdelrahman Omran <me@omranic.com>
2019-08-03 04:01:52 +0400

    Tweak menus & breadcrumbs performance

--- src/Providers/TenantsServiceProvider.php
+++ src/Providers/TenantsServiceProvider.php
@@ -73,12 +73,13 @@ class TenantsServiceProvider extends ServiceProvider
         ]);
 
         // Load resources
-        require __DIR__.'/../../routes/breadcrumbs/adminarea.php';
         $this->loadRoutesFrom(__DIR__.'/../../routes/web/adminarea.php');
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'cortex/tenants');
         $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'cortex/tenants');
         $this->app->runningInConsole() || $this->app->afterResolving('blade.compiler', function () {
-            require __DIR__.'/../../routes/menus/adminarea.php';
+            $accessarea = $this->app['request']->route('accessarea');
+            ! file_exists($menus = __DIR__."/../../routes/menus/{$accessarea}.php") || require $menus;
+            ! file_exists($breadcrumbs = __DIR__."/../../routes/breadcrumbs/{$accessarea}.php") || require $breadcrumbs;
         });
 
         // Inject tenantable middleware before route bindings substitution
