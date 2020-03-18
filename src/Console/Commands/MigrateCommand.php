4b5bf4d423234ca54586e6a49e80012da0c93d6d
Abdelrahman Omran <me@omranic.com>
2020-03-18 15:18:02 +0400

    Fix migrations path condition

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -31,7 +31,11 @@ class MigrateCommand extends BaseMigrateCommand
     {
         parent::handle();
 
-        if (file_exists($path = 'database/migrations/cortex/tenants')) {
+        $path = config('cortex.tenants.autoload_migrations') ?
+            'app/cortex/tenants/database/migrations' :
+            'database/migrations/cortex/tenants';
+
+        if (file_exists($path)) {
             $this->call('migrate', [
                 '--step' => true,
                 '--path' => $path,
--- src/Console/Commands/RollbackCommand.php
+++ src/Console/Commands/RollbackCommand.php
@@ -29,7 +29,11 @@ class RollbackCommand extends BaseRollbackCommand
      */
     public function handle(): void
     {
-        if (file_exists($path = 'database/migrations/cortex/tenants')) {
+        $path = config('cortex.tenants.autoload_migrations') ?
+            'app/cortex/tenants/database/migrations' :
+            'database/migrations/cortex/tenants';
+
+        if (file_exists($path)) {
             $this->call('migrate:reset', [
                 '--path' => $path,
                 '--force' => $this->option('force'),
