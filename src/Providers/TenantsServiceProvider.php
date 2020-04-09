aa8e36cbaf89330d61ab9668c7a43d31211325e5
Abdelrahman Omran <me@omranic.com>
2020-04-09 09:29:51 +0400

    Tweak artisan command registration

--- src/Providers/TenantsServiceProvider.php
+++ src/Providers/TenantsServiceProvider.php
@@ -54,7 +54,7 @@ class TenantsServiceProvider extends ServiceProvider
         || $this->app->alias('rinvex.tenants.tenant', Tenant::class);
 
         // Register console commands
-        ! $this->app->runningInConsole() || $this->registerCommands();
+        $this->registerCommands();
     }
 
     /**
