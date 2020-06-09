238b1005510a1350a5f93093550b9b71ae0392b4
Abdelrahman Omran <me@omranic.com>
2020-06-09 21:27:04 +0400

    Only display tenant edit menu if manager has permission to update

--- routes/menus/managerarea.php
+++ routes/menus/managerarea.php
@@ -8,6 +8,6 @@ use Rinvex\Menus\Models\MenuGenerator;
 Menu::register('managerarea.header.user', function (MenuGenerator $menu) {
     $tenant = config('rinvex.tenants.active');
     $menu->dropdown(function (MenuItem $dropdown) use ($tenant) {
-        $dropdown->route(['managerarea.tenants.edit'], trans('cortex/auth::common.settings'), 10, 'fa fa-building-o');
+        $dropdown->route(['managerarea.tenants.edit'], trans('cortex/auth::common.settings'), 10, 'fa fa-building-o')->ifCan('update', $tenant);
     }, $tenant->name, 9, 'fa fa-briefcase');
 });
