f78b1247b7d6fa22016550c17d4c5bb7da141806
Abdelrahman Omran <me@omranic.com>
2020-06-21 17:27:27 +0400

    Only query the tenant if we have a subdomain

--- bootstrap/module.php
+++ bootstrap/module.php
@@ -6,7 +6,7 @@ return function () {
     // Discover and set current active tenant. We need `request.tenant` as early as we can, since
     // it's used in bootable traits, routes, and other resources called in service providers!
     $subdomain = mb_strstr($this->app->request->getHost(), '.'.domain(), true);
-    $tenant = app('rinvex.tenants.tenant')->where('slug', (string) $subdomain)->first();
+    $tenant = $subdomain ? app('rinvex.tenants.tenant')->where('slug', (string) $subdomain)->first() : null;
     $this->app->singleton('request.subdomain', fn () => $subdomain);
     $this->app->singleton('request.tenant', fn () => $tenant);
 };
