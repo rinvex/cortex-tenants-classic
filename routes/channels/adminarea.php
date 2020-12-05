fbfa22a592e3b7bead0c0526ca3046dd979e1de0
Abdelrahman Omran <me@omranic.com>
2020-12-05 12:19:32 +0400

    Type hint Authorizable user parameter

--- routes/channels/adminarea.php
+++ routes/channels/adminarea.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
-Broadcast::channel('adminarea-tenants-index', function ($user) {
+use Illuminate\Contracts\Auth\Access\Authorizable;
+
+Broadcast::channel('adminarea-tenants-index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.tenants.tenant'));
 }, ['guards' => ['admin']]);
