a10378dd9d5313f80fbdb867e6f544e3d66303e5
Abdelrahman Omran <Omranic@users.noreply.github.com>
2020-12-25 02:56:46 +0000

    Apply fixes from StyleCI

--- routes/breadcrumbs/adminarea.php
+++ routes/breadcrumbs/adminarea.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 use Cortex\Tenants\Models\Tenant;
-use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator;
+use Diglactic\Breadcrumbs\Breadcrumbs;
 
 Breadcrumbs::register('adminarea.cortex.tenants.tenants.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('adminarea.home'));
