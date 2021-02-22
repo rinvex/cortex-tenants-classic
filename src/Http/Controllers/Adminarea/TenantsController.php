99b50f1f4f39d5558b08e9d89cca49eadea7136e
Abdelrahman Omran <me@omranic.com>
2021-02-23 03:51:07 +0400

    Use overridden `FormRequest` instead of native class

--- src/Http/Controllers/Adminarea/TenantsController.php
+++ src/Http/Controllers/Adminarea/TenantsController.php
@@ -7,7 +7,7 @@ namespace Cortex\Tenants\Http\Controllers\Adminarea;
 use Exception;
 use Illuminate\Http\Request;
 use Cortex\Tenants\Models\Tenant;
-use Illuminate\Foundation\Http\FormRequest;
+use Cortex\Foundation\Http\FormRequest;
 use Cortex\Foundation\DataTables\LogsDataTable;
 use Cortex\Foundation\Importers\DefaultImporter;
 use Cortex\Foundation\DataTables\ImportLogsDataTable;
@@ -219,7 +219,7 @@ class TenantsController extends AuthorizedController
     /**
      * Process stored/updated tenant.
      *
-     * @param \Illuminate\Foundation\Http\FormRequest $request
+     * @param \Cortex\Foundation\Http\FormRequest $request
      * @param \Cortex\Tenants\Models\Tenant           $tenant
      *
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
