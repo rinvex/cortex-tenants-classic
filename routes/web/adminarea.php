d6ca466fd3568d8d35f00cc99b78fbfde9661fdb
Abdelrahman Omran <me@omranic.com>
2021-08-05 00:13:27 +0400

    Simplify route prefixes

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -6,7 +6,7 @@ Route::domain(domain())->group(function () {
     Route::name('adminarea.')
          ->namespace('Cortex\Tenants\Http\Controllers\Adminarea')
          ->middleware(['web', 'nohttpcache', 'can:access-adminarea'])
-         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.adminarea') : config('cortex.foundation.route.prefix.adminarea'))->group(function () {
+         ->prefix(route_prefix('adminarea'))->group(function () {
 
         // Tenants Routes
              Route::name('cortex.tenants.tenants.')->prefix('tenants')->group(function () {
--- routes/web/managerarea.php
+++ routes/web/managerarea.php
@@ -6,7 +6,7 @@ Route::domain('{subdomain}.'.domain())->group(function () {
     Route::name('managerarea.')
          ->namespace('Cortex\Tenants\Http\Controllers\Managerarea')
          ->middleware(['web', 'nohttpcache', 'can:access-managerarea'])
-         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.managerarea') : config('cortex.foundation.route.prefix.managerarea'))->group(function () {
+         ->prefix(route_prefix('managerarea'))->group(function () {
 
              // Managerarea Home route
              Route::name('cortex.tenants.tenants.')->prefix('tenants')->group(function () {
