504585ffbaeaab099cc71e1c5d1612a1766ae6f6
Abdelrahman Omran <me@omranic.com>
2021-08-18 09:05:39 +0400

    Move tenant retrieval and registration to rinvex/laravel-tenants responsibility

--- bootstrap/module.php
+++ bootstrap/module.php
@@ -6,11 +6,4 @@ return function () {
     // Bind route models and constrains
     Route::pattern('tenant', '[a-zA-Z0-9-_]+');
     Route::model('tenant', config('rinvex.tenants.models.tenant'));
-
-    // Discover and set current active tenant. We need `request.tenant` as early as we can, since
-    // it's used in bootable traits, routes, and other resources called in service providers!
-    $subdomain = mb_strstr($this->app->request->getHost(), '.'.domain(), true);
-    $tenant = $subdomain ? app('rinvex.tenants.tenant')->where('slug', (string) $subdomain)->where('is_active', true)->first() : null;
-    $this->app->singleton('request.subdomain', fn () => $subdomain);
-    $this->app->singleton('request.tenant', fn () => $tenant);
 };
