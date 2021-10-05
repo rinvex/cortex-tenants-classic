d9271abb0d70a7d694d182ccd09e339d295b3e1f
Abdelrahman Omran <me@omranic.com>
2021-10-06 03:51:59 +0400

    Rename variables for consistency

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -10,9 +10,9 @@ if (! function_exists('central_pattern')) {
      */
     function central_pattern()
     {
-        $centralDomainPatterns = implode('|', central_domains());
+        $centralDomains = implode('|', central_domains());
 
-        return "^(${centralDomainPatterns})$";
+        return "^(${centralDomains})$";
     }
 }
 
@@ -24,8 +24,8 @@ if (! function_exists('tenant_pattern')) {
      */
     function tenant_pattern()
     {
-        $cetralDomains = central_pattern();
+        $centralPattern = central_pattern();
 
-        return "^((?!-|${cetralDomains})[a-zA-Z0–9-]{1,63}(?<!-)\\.)+[a-zA-Z]{2,63}$";
+        return "^((?!-|${centralPattern})[a-zA-Z0–9-]{1,63}(?<!-)\\.)+[a-zA-Z]{2,63}$";
     }
 }
