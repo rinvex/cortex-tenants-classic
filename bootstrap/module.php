40c72ff5a9343aec34025909028893f4790087ce
Abdelrahman Omran <me@omranic.com>
2021-10-10 00:37:36 +0400

    Register routeDomain pattern

--- bootstrap/module.php
+++ bootstrap/module.php
@@ -5,5 +5,6 @@ declare(strict_types=1);
 return function () {
     // Bind route models and constrains
     Route::pattern('tenant', '[a-zA-Z0-9-_]+');
+    Route::pattern('routeDomain', route_domains_pattern());
     Route::model('tenant', config('rinvex.tenants.models.tenant'));
 };
