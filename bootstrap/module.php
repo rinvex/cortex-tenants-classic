00be041b0ac107e31b4201f247ba646128f1b975
Abdelrahman Omran <me@omranic.com>
2021-10-10 00:38:01 +0400

    Override app.url & session.domain config options

--- bootstrap/module.php
+++ bootstrap/module.php
@@ -7,4 +7,18 @@ return function () {
     Route::pattern('tenant', '[a-zA-Z0-9-_]+');
     Route::pattern('routeDomain', route_domains_pattern());
     Route::model('tenant', config('rinvex.tenants.models.tenant'));
+
+    $centralDomains = route_domains();
+    $domain = $this->app['request']->getHost();
+    $scheme = $this->app['request']->getScheme();
+
+    // Dynamically change session domain config on the fly
+    if (in_array($domain, array_merge([optional($this->app['request.tenant'])->domain], $centralDomains))) {
+        config()->set('session.domain', '.'.$domain);
+        config()->set('app.url', $scheme.'://'.$domain);
+    } elseif (Str::endsWith($domain, $centralDomains)) {
+        $sessionDomain = Arr::first($centralDomains, fn ($tld) => Str::endsWith($domain, $tld));
+        config()->set('session.domain', '.'.$sessionDomain);
+        config()->set('app.url', $scheme.'://'.$domain);
+    }
 };
