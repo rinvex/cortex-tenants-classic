43c6852b03f933b330753bf949dedfa72e43be0e
Abdelrahman Omran <me@omranic.com>
2022-02-02 10:39:20 +0400

    Fix broadcasts naming convensions

--- routes/broadcasts/channels.php
+++ routes/broadcasts/channels.php
@@ -6,4 +6,4 @@ use Illuminate\Contracts\Auth\Access\Authorizable;
 
 Broadcast::channel('cortex.tenants.tenants.index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.tenants.tenant'));
-}, ['guards' => ['admin']]);
+});
