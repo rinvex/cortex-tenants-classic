b29e344908d5e74fb90313175e5ad80be043a9ae
Abdelrahman Omran <me@omranic.com>
2022-02-02 10:38:10 +0400

    Update routes to use class based definitions

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -2,31 +2,33 @@
 
 declare(strict_types=1);
 
+use Cortex\Tenants\Http\Controllers\Adminarea\TenantsController;
+use Cortex\Tenants\Http\Controllers\Adminarea\TenantsMediaController;
+
 Route::domain('{adminarea}')->group(function () {
     Route::name('adminarea.')
-         ->namespace('Cortex\Tenants\Http\Controllers\Adminarea')
          ->middleware(['web', 'nohttpcache', 'can:access-adminarea'])
          ->prefix(route_prefix('adminarea'))->group(function () {
 
         // Tenants Routes
              Route::name('cortex.tenants.tenants.')->prefix('tenants')->group(function () {
-                 Route::match(['get', 'post'], '/')->name('index')->uses('TenantsController@index');
-                 Route::get('import')->name('import')->uses('TenantsController@import');
-                 Route::post('import')->name('stash')->uses('TenantsController@stash');
-                 Route::post('hoard')->name('hoard')->uses('TenantsController@hoard');
-                 Route::get('import/logs')->name('import.logs')->uses('TenantsController@importLogs');
-                 Route::get('create')->name('create')->uses('TenantsController@create');
-                 Route::post('create')->name('store')->uses('TenantsController@store');
-                 Route::get('{tenant}')->name('show')->uses('TenantsController@show');
-                 Route::get('{tenant}/edit')->name('edit')->uses('TenantsController@edit');
-                 Route::put('{tenant}/edit')->name('update')->uses('TenantsController@update');
-                 Route::match(['get', 'post'], '{tenant}/logs')->name('logs')->uses('TenantsController@logs');
-                 Route::delete('{tenant}')->name('destroy')->uses('TenantsController@destroy');
+                 Route::match(['get', 'post'], '/')->name('index')->uses([TenantsController::class, 'index']);
+                 Route::get('import')->name('import')->uses([TenantsController::class, 'import']);
+                 Route::post('import')->name('stash')->uses([TenantsController::class, 'stash']);
+                 Route::post('hoard')->name('hoard')->uses([TenantsController::class, 'hoard']);
+                 Route::get('import/logs')->name('import.logs')->uses([TenantsController::class, 'importLogs']);
+                 Route::get('create')->name('create')->uses([TenantsController::class, 'create']);
+                 Route::post('create')->name('store')->uses([TenantsController::class, 'store']);
+                 Route::get('{tenant}')->name('show')->uses([TenantsController::class, 'show']);
+                 Route::get('{tenant}/edit')->name('edit')->uses([TenantsController::class, 'edit']);
+                 Route::put('{tenant}/edit')->name('update')->uses([TenantsController::class, 'update']);
+                 Route::match(['get', 'post'], '{tenant}/logs')->name('logs')->uses([TenantsController::class, 'logs']);
+                 Route::delete('{tenant}')->name('destroy')->uses([TenantsController::class, 'destroy']);
 
                  Route::name('media.')->prefix('{tenant}/media')->group(function () {
-                     Route::get('/')->name('index')->uses('TenantsMediaController@index');
-                     Route::post('/')->name('store')->uses('TenantsMediaController@store');
-                     Route::delete('{media}')->name('destroy')->uses('TenantsMediaController@destroy');
+                     Route::get('/')->name('index')->uses([TenantsMediaController::class, 'index']);
+                     Route::post('/')->name('store')->uses([TenantsMediaController::class, 'store']);
+                     Route::delete('{media}')->name('destroy')->uses([TenantsMediaController::class, 'destroy']);
                  });
              });
          });
--- routes/web/managerarea.php
+++ routes/web/managerarea.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
+use Cortex\Tenants\Http\Controllers\Managerarea\TenantsMediaController;
+
 Route::domain('{managerarea}')->group(function () {
     Route::name('managerarea.')
-         ->namespace('Cortex\Tenants\Http\Controllers\Managerarea')
          ->middleware(['web', 'nohttpcache', 'can:access-managerarea'])
          ->prefix(route_prefix('managerarea'))->group(function () {
 
@@ -12,8 +13,9 @@ Route::domain('{managerarea}')->group(function () {
              Route::name('cortex.tenants.tenants.')->prefix('tenants')->group(function () {
                  Route::get('edit')->name('edit')->uses('TenantsController@edit');
                  Route::put('edit')->name('update')->uses('TenantsController@update');
+
                  Route::name('media.')->prefix('media')->group(function () {
-                     Route::delete('{media}')->name('destroy')->uses('TenantsMediaController@destroy');
+                     Route::delete('{media}')->name('destroy')->uses([TenantsMediaController::class, 'destroy']);
                  });
              });
          });
