e9c098f2645eab31ee84d3159a002c13808d3188
Abdelrahman Omran <me@omranic.com>
2022-05-15 14:17:44 +0300

    Apply fixes from StyleCI (#156)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- routes/menus/adminarea.php
+++ routes/menus/adminarea.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Cortex\Tenants\Models\Tenant;
 use Rinvex\Menus\Models\MenuItem;
-use Rinvex\Menus\Models\MenuGenerator;
 use Cortex\Foundation\Models\Media;
+use Rinvex\Menus\Models\MenuGenerator;
 
 Menu::register('adminarea.sidebar', function (MenuGenerator $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.crm'), 50, 'fa fa-briefcase', 'header', [], function (MenuItem $dropdown) {
