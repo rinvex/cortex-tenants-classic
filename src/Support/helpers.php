e75bf775c694f5436494bcb61e6dedc0d0145651
Abdelrahman Omran <me@omranic.com>
2022-12-07 06:23:57 +0400

    Apply fixes from StyleCI (#158)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -20,13 +20,14 @@ if (! function_exists('route_domains')) {
 
         $routeDomains = $accessarea ? collect(config('app.domains'))->filter(fn ($accessareas) => in_array($accessarea, $accessareas))->keys() : collect(config('app.domains'))->keys();
 
-        if(app()->has('request.tenant') && app('request.tenant') && in_array($accessarea, ['managerarea', 'tenantarea'])){
+        if (app()->has('request.tenant') && app('request.tenant') && in_array($accessarea, ['managerarea', 'tenantarea'])) {
             $routeDomains = $routeDomains->map(fn ($routeDomain) => app('request.tenant')->slug.'.'.$routeDomain);
-            
-            if(! empty(app('request.tenant')->domain)) {
+
+            if (! empty(app('request.tenant')->domain)) {
                 $routeDomains->prepend(app('request.tenant')->domain);
             }
         }
+
         return $cachedDomains[$accessarea] = $routeDomains->toArray();
     }
 }
