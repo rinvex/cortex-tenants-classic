61d7aa42d2a22020875c63518f276e37f2ba68e3
Abdelrahman Omran <me@omranic.com>
2023-06-05 21:21:59 +0400

    Decoupling: Move global helpers, route patterns and middleware to cortex/foundation module

--- bootstrap/module.php
+++ bootstrap/module.php
@@ -2,19 +2,10 @@
 
 declare(strict_types=1);
 
-use Cortex\Tenants\Http\Middleware\SetSessionConfigRuntime;
-
 return function () {
     // Bind route models and constrains
     Route::pattern('tenant', '[a-zA-Z0-9-_]+');
-    Route::pattern('absentarea', '^([a-zA-Z0-9\-\.]+)$');
-    Route::pattern('centralarea', route_pattern());
-    Route::pattern('frontarea', route_pattern('frontarea'));
-    Route::pattern('adminarea', route_pattern('adminarea'));
     Route::pattern('tenantarea', route_pattern('tenantarea'));
     Route::pattern('managerarea', route_pattern('managerarea'));
     Route::model('tenant', config('rinvex.tenants.models.tenant'));
-
-    // prepend middleware to the 'web' middleware group
-    Route::prependMiddlewareToGroup('web', SetSessionConfigRuntime::class);
 };
